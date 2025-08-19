0aa4488d3b1c954231b4a1343a357a4afb5a3f86
woernoe <we@ernstware.de>
2025-08-19 12:43:17 +0200

    Ass 3 V 01

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -23,6 +23,8 @@ fi
 
 mkdir -p ${OUTDIR}
 
+
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
@@ -35,9 +37,29 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+
+    # clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper 
+    # insert make defconfig
+    make ARCH=${ARCH}  CROSS_COMPILE=${CROSS_COMPILE}  defconfig
+
+    # here defconfig used
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+    
+    # build
+    make -j4  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    # Included in male ... all
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}  modules
+
+    # Included in male ... all
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+   
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
+
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +69,14 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+
 # TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +84,73 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
+
     # TODO:  Configure busybox
+    
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} menuconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig      # default configuration
+
 else
     cd busybox
 fi
 
+
 # TODO: Make and install busybox
+# -> see Config   make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+# -> see config   make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
+
 
 # TODO: Add library dependencies to rootfs
+cp /home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp /home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp /home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
-# TODO: Make device nodes
+
+# TODO: Make device nodes 
+cd ${OUTDIR}/rootfs/dev
+sudo mknod -m 622 console c 5 1
+sudo mknod -m 666 null c 1 3
+
 
 # TODO: Clean and build the writer utility
+cd ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app
+
+
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} all
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/writer ${OUTDIR}/rootfs/home
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/conf/username.txt ${OUTDIR}/rootfs/home/conf
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/finder.sh ${OUTDIR}/rootfs/home
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/finder-test.sh ${OUTDIR}/rootfs/home
+cp ~/CU/assignments/assignment-3-and-later/assignment-3-and-later-woernoe/finder-app/autorun-qemu.sh ${OUTDIR}/rootfs/home
+
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root .
+
 
 # TODO: Create initramfs.cpio.gz
+#find . -print0 | cpio --null -ov --format=newc | gzip -n > ../initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+
+
+
