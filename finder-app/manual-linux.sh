6051e790a6861b5565428b4f5fc269e67cb44d0b
woernoe <we@ernstware.de>
2025-08-19 23:37:51 +0200

    Ass3 V1.8

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -30,7 +30,9 @@ mkdir -p ${OUTDIR}
 USER_NAM=$(whoami)
 echo "User -> ${USER_NAM}"
 
-PATHLIB=/home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib
+#PATHLIB=/home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib
+PATHLIB=/home/ernst/CU/assignments/assignment-3-and-later/lib
+echo "try sudo cp"
 sudo cp ${PATHLIB}/ld-linux-aarch64.so.1 ${OUTDIR}/
 echo "with sudo fine !!!"
 cp ${PATHLIB}/ld-linux-aarch64.so.1 ${OUTDIR}/
