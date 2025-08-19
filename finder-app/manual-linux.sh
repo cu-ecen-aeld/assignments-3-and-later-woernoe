cf750b9ad623ec9c5f8e69d20b7c32b7d69c87b1
woernoe <we@ernstware.de>
2025-08-19 22:54:59 +0200

    Ass 3 V01.3

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -26,6 +26,9 @@ fi
 mkdir -p ${OUTDIR}
 
 
+#testtt
+cp /home/ernst/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
