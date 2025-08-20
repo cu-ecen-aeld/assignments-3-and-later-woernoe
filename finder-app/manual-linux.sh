7e9d0907da41626670a0d893f5e4ab857a7a1662
woernoe <we@ernstware.de>
2025-08-20 17:35:32 +0200

    Ass 3 V1.20

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -60,16 +60,16 @@ else
 fi
 
 
-echo "./finder-app/libs:"
-ls -l ./finder-app/libs
+echo "finder-app/libs:"
+ls -l finder-app/libs
 echo "===="
 
 echo "./finder-app/libs/lib"
-ls -l ./finder-app/libs/lib
+ls -l finder-app/libs/lib
 echo "=========="
 
 echo "./libs/lib64"
-ls -l ./finder-app/libs/lib64
+ls -l finder-app/libs/lib64
 echo "======"
 
 echo "/usr"
