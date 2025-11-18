af9f6b7af017a83603e3f11cbc8b42f1fe4ba70a
woernoe <we@ernstware.de>
2025-11-18 15:55:25 +0100

    Assignment 8 V08

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -169,7 +169,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
              if (pEntry == NULL )  {
                  kfree( dev->tmpData);
                  dev->tmpData = NULL;
-                 dev->TmpDataSize = 0;
+                 dev->tmpDataSize = 0;
                  goto out;
              } 
              
